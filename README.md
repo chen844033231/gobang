f85da07e7d4edc835367082b9e456862452f7fe1
marchen <844033231@qq.com>
2016-05-11 10:23:56 +0800

    采用面向对象方法编写

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gobang
 canvas单机版五子棋
+
+带胜负判断 改变script/chess.js 里面的 isSupportCanvas变量即可改变渲染方式(默认canvas渲染, 不支持会改为dom渲染)
--- script/chess.js
+++ script/chess.js
@@ -1,98 +1,121 @@
 /////////////////////////////////////////////////////////////////////
-//  兼容方法
+//  棋子, 棋盘类
 /////////////////////////////////////////////////////////////////////
-//var isSupportCanvas = !!document.createElement("canvas").getContext
-var isSupportCanvas = true  // 直接改变该变量及可以改变canvas和dom互换
+var isSupportCanvas = !!document.createElement("canvas").getContext  // 直接改变该变量及可以改变canvas和dom互换
+//var isSupportCanvas = false 
+
 /**
- * [绘制棋子]
- * @game [dom] 容器
- * @return dom 棋盘
+ * [棋子类]
+ * @game [dom] 游戏真实dom
+ * @x [number] x位置
+ * @y [number] y位置
+ * @color [string] 颜色
  */
-var drawChess = (function(){
-	if(isSupportCanvas) {
-		return function(chessboard, player, x, y) {
-			var ctx = chessboard.getContext('2d')
-			var color = (player === -1 ? '#000000' : '#ffffff')
-			ctx.fillStyle = color
-			ctx.beginPath()
-			ctx.arc(x * 30 + 15, y * 30 + 15, 12, 0, Math.PI * 2)
-			ctx.fill()
-		}
-	} else {
-		return function(chessboard, player, x, y) {
-			var chessman = document.createElement('div')
-			var color = (player === -1 ? '#000000' : '#ffffff')
-			chessman.style.width = '24px'
-			chessman.style.height = '24px'
-			chessman.style.borderRaduis = '50%'
-			chessman.style.webkitBorderRadius = '50%'
-			chessman.style.backgroundColor = color
-			chessman.style.position = 'absolute'
-			chessman.style.top = y * 30 + 3 + 'px'
-			chessman.style.left = x * 30 + 3 + 'px'
-			chessboard.appendChild(chessman)
-		}
-	}
+function Chessman(game, x, y, color) {
+    this.game = game
+    this.x = x || 0
+    this.y = y || 0
+    this.color = color || '#000000'
+}
+
+// 动态渲染方法
+Chessman.prototype.render = (function() {
+    if (isSupportCanvas) {
+        return function() {
+            var ctx = this.game.getContext('2d')
+            ctx.fillStyle = this.color
+            ctx.beginPath()
+            ctx.arc(this.x * 30 + 15, this.y * 30 + 15, 12, 0, Math.PI * 2)
+            ctx.fill()
+            ctx.closePath()
+            return ctx
+        }
+    } else {
+        return function() {
+            var chessman = document.createElement('div')
+            chessman.style.width = '24px'
+            chessman.style.height = '24px'
+            chessman.style.borderRaduis = '50%'
+            chessman.style.webkitBorderRadius = '50%'
+            chessman.style.backgroundColor = this.color
+            chessman.style.position = 'absolute'
+            chessman.style.top = this.y * 30 + 3 + 'px'
+            chessman.style.left = this.x * 30 + 3 + 'px'
+            this.game.appendChild(chessman)
+            return chessman
+        }
+    }
 })()
 
+
 /**
- * [绘制棋盘]
- * @game [dom] 容器,默认为id
- * @return dom 棋盘
+ * [棋盘类]
+ * @container [string] 容器id
+ * @width [number] 棋盘宽度 默认正方形棋盘
+ * @game [dom] 真实游戏dom
  */
-var drawChessboard = (function(id){
-	var boardWidth = 900
-	var game = document.getElementById(game || 'game')
-	if(isSupportCanvas) {
-		var canvas = document.createElement('canvas')  // 创建canvas元素
-		canvas.width = boardWidth
-		canvas.height = boardWidth
-		canvas.style.background = '#AFB14B'
-		game.appendChild(canvas)
-		var ctx = canvas.getContext('2d')
-		return function() {
-			for (var i = 0; i <= boardWidth; i = i + 30) {
-				ctx.strokeStyle = '#313131'
-	      ctx.beginPath()
-	      ctx.moveTo(0, i)
-	      ctx.lineTo(boardWidth, i)
-	      ctx.stroke()
+function Chessboard(container, width, game) {
+    this.container = document.getElementById(container || 'container') //容器
+    this.width = width || 900 // 宽度
+    this.game = game || '' // 真实游戏dom
+}
 
-	      ctx.beginPath()
-	      ctx.moveTo(i, 0)
-	      ctx.lineTo(i, boardWidth)
-	      ctx.stroke()
-      }
-      return canvas
-		}
-	} else {
-		var dom = document.createElement('div')  // 创建dom元素
-		dom.style.width = boardWidth + 'px'
-		dom.style.height = boardWidth + 'px'
-		dom.style.position = 'relative'
-		dom.style.backgroundColor = '#AFB14B'
-		game.appendChild(dom)
-		return function(id) {
-			for (var i = 0; i <= boardWidth; i = i + 30) {
-				var lineH = document.createElement('div')
-				lineH.style.width = '100%'
-				lineH.style.height = '1px'
-				lineH.style.backgroundColor = '#313131'
-				lineH.style.position = 'absolute'
-				lineH.style.top = i + 'px'
-				lineH.style.left = 0
-				dom.appendChild(lineH)
+// 渲染棋盘
+Chessboard.prototype.render = (function() {
+    if (isSupportCanvas) {
+        return function() {
+            var canvas = document.createElement('canvas') // 创建canvas元素
+            var boardWidth = this.width
+            canvas.width = boardWidth
+            canvas.height = boardWidth
+            canvas.style.background = '#AFB14B'
+            this.container.appendChild(canvas)
+            this.game = canvas
+            var ctx = canvas.getContext('2d')
+            for (var i = 0; i <= boardWidth; i = i + 30) {
+                ctx.strokeStyle = '#313131'
+                ctx.beginPath()
+                ctx.moveTo(0, i)
+                ctx.lineTo(boardWidth, i)
+                ctx.stroke()
 
-				var lineV = document.createElement('div')
-				lineV.style.width = '1px'
-				lineV.style.height = '100%'
-				lineV.style.backgroundColor = '#313131'
-				lineV.style.position = 'absolute'
-				lineV.style.top = 0
-				lineV.style.left = i + 'px'
-				dom.appendChild(lineV)
-      }
-			return dom
-		}
-	}
+                ctx.beginPath()
+                ctx.moveTo(i, 0)
+                ctx.lineTo(i, boardWidth)
+                ctx.stroke()
+            }
+            return canvas
+        }
+    } else {
+        return function() {
+            var dom = document.createElement('div') // 创建dom元素
+            var boardWidth = this.width
+            dom.style.width = boardWidth + 'px'
+            dom.style.height = boardWidth + 'px'
+            dom.style.position = 'relative'
+            dom.style.backgroundColor = '#AFB14B'
+            this.container.appendChild(dom)
+            this.game = dom
+            for (var i = 0; i <= boardWidth; i = i + 30) {
+                var lineH = document.createElement('div')
+                lineH.style.width = '100%'
+                lineH.style.height = '1px'
+                lineH.style.backgroundColor = '#313131'
+                lineH.style.position = 'absolute'
+                lineH.style.top = i + 'px'
+                lineH.style.left = 0
+                dom.appendChild(lineH)
+
+                var lineV = document.createElement('div')
+                lineV.style.width = '1px'
+                lineV.style.height = '100%'
+                lineV.style.backgroundColor = '#313131'
+                lineV.style.position = 'absolute'
+                lineV.style.top = 0
+                lineV.style.left = i + 'px'
+                dom.appendChild(lineV)
+            }
+            return dom
+        }
+    }
 })()
